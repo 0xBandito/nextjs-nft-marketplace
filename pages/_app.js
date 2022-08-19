7a3aa758df0ab9e5f4281b21dfc4fb30f05ac0b4
0xBandito <0xbandito@0xBanditos-MBP.attlocal.net>
2022-08-19 16:24:22 -0400

    added events to Moralis DB

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import {MoralisProvider} from "react-moralis"
+import Header from "../components/Header";
+import Head from "next/head"
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_MORALIS_SERVER
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>NFT MARKETPLACE</title>
+        <meta name="description" content="NFT MARKETPLACE" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+      <Header />
+      <Component {...pageProps} />
+    </MoralisProvider>
+    </div>
+    
+    )
 }
 
 export default MyApp
